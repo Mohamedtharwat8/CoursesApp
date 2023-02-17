ce2b9ff3d856062e2f5a50274f4aef5523fa3fbc
Mohamed Tharwat <114985142+Mohamedtharwat8@users.noreply.github.com>
2023-02-17 11:29:24 +0200

    Add files And Areas

--- CoursesApp/App_Start/RouteConfig.cs
+++ CoursesApp/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace CoursesApp
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                new string[] { "CoursesApp.Controllers" }
             );
         }
     }
---
+++ CoursesApp/Models/CourseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoursesApp.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+
+
+    }
+}
